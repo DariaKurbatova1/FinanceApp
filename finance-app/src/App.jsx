123544d4ad2926741589f828fe7d127063f2de68
DariaKurbatova1 <kurbatova.daria@gmail.com>
2025-01-07 11:07:45 -0500

    add login page

--- finance-app/src/App.jsx
+++ finance-app/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header'
-
+import LoginPage from './components/LoginPage';
 function App() {
 
   return (
     <>
+    <Router>
       <h1>Finance App</h1>
       <Header/>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      </Router>
     </>
   )
 }
--- finance-app/src/components/Header.jsx
+++ finance-app/src/components/Header.jsx
@@ -4,11 +4,11 @@ function Header(){
     <header>
       <nav>
         <ul>
-          <li><a href="#login">Login</a></li>
-          <li><a href="#expenses">Expense Tracking</a></li>
-          <li><a href="#budgeting">Budgeting</a></li>
-          <li><a href="#income">Income Tracking</a></li>
-          <li><a href="#goals">Financial Goals</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/expenses">Expense Tracking</a></li>
+          <li><a href="/budgeting">Budgeting</a></li>
+          <li><a href="/income">Income Tracking</a></li>
+          <li><a href="/goals">Financial Goals</a></li>
         </ul>
       </nav>
     </header>
