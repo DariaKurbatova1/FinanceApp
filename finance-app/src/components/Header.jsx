ff4864ded8a9f369d9aa1690b05011acf8331572
DariaKurbatova1 <kurbatova.daria@gmail.com>
2025-01-07 12:35:42 -0500

    handle whether user is logged in/out and display login/logout accordingly

--- finance-app/src/components/Header.jsx
+++ finance-app/src/components/Header.jsx
@@ -1,10 +1,21 @@
 
 function Header(){
+  const handleLoginLogout = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      localStorage.removeItem('token');
+      window.location.href = '/login'; 
+    } else {
+      window.location.href = '/login';
+    }
+  };
+  const isLoggedIn = localStorage.getItem('token');
+
   return (
     <header>
       <nav>
         <ul>
-          <li><a href="/login">Login</a></li>
+        <li><a href="#" onClick={handleLoginLogout}>{isLoggedIn ? 'Logout' : 'Login'}</a></li>
           <li><a href="/expenses">Expense Tracking</a></li>
           <li><a href="/budgeting">Budgeting</a></li>
           <li><a href="/income">Income Tracking</a></li>
