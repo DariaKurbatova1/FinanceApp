87d4d7b3435ea9cfac3fe0acf256671234c2f81c
DariaKurbatova1 <kurbatova.daria@gmail.com>
2025-01-07 11:11:18 -0500

    make h1 a link to the homepage

--- finance-app/src/App.jsx
+++ finance-app/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from './components/Header'
 import LoginPage from './components/LoginPage';
 function App() {
@@ -8,7 +8,11 @@ function App() {
   return (
     <>
     <Router>
-      <h1>Finance App</h1>
+      <header>
+        <Link to="/">
+          <h1>Finance App</h1> 
+        </Link>
+      </header>
       <Header/>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
