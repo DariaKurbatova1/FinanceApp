72e24b56cd0b87af6fa900ab5273641b3483a719
DariaKurbatova1 <kurbatova.daria@gmail.com>
2025-01-07 10:56:32 -0500

    add header component

---
+++ finance-app/src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header(){
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#login">Login</a></li>
+          <li><a href="#expenses">Expense Tracking</a></li>
+          <li><a href="#budgeting">Budgeting</a></li>
+          <li><a href="#income">Income Tracking</a></li>
+          <li><a href="#goals">Financial Goals</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
