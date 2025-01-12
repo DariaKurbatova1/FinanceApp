66b85ff0c846669119534830c9e3f256bfbeba12
DariaKurbatova1 <kurbatova.daria@gmail.com>
2025-01-12 15:12:28 -0500

    make all components unvailable unless user is logged in

--- finance-app/src/App.jsx
+++ finance-app/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header'
 import LoginPage from './components/LoginPage';
 import ExpenseTracking from './components/ExpenseTracking';
@@ -9,6 +9,8 @@ import IncomeTracking from './components/IncomeTracking';
 import FinancialGoals from './components/FinancialGoals';
 import HomePage from './components/HomePage'
 function App() {
+
+  const isLoggedIn = Boolean(localStorage.getItem('token'));
 
   return (
     <>
@@ -26,10 +28,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/expenses" element={<ExpenseTracking />} />
-        <Route path="/budgeting" element={<Budgeting />} />
-        <Route path="/income" element={<IncomeTracking />} />
-        <Route path="/goals" element={<FinancialGoals />} />
+        <Route path="/expenses" element={isLoggedIn ? <ExpenseTracking /> : <Navigate to="/login" />} />
+          <Route path="/budgeting" element={isLoggedIn ? <Budgeting /> : <Navigate to="/login" />} />
+          <Route path="/income" element={isLoggedIn ? <IncomeTracking /> : <Navigate to="/login" />} />
+          <Route path="/goals" element={isLoggedIn ? <FinancialGoals /> : <Navigate to="/login" />} />
       </Routes>
       </Router>
 
