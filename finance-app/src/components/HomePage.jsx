0e79a4bc68bc34526fa4b331e09999e58cf52d87
DariaKurbatova1 <kurbatova.daria@gmail.com>
2025-01-14 11:19:31 -0500

    add conditional rendering for get started button

--- finance-app/src/components/HomePage.jsx
+++ finance-app/src/components/HomePage.jsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function HomePage(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        setIsLoggedIn(token); 
+      }, []);
+
     return (
         <>
         <div className="home-intro">
             <h2>Welcome to Your Personal Finance Tracker</h2>
             <p>Keep track of your income, expenses, goals, and budgeting in one place.</p>
         </div>
-        <div className="get-started">
+        {!isLoggedIn && (
+            <div className="get-started">
             <h3>Start Tracking Your Finances Today!</h3>
-            <a href="/login" className="get-started-button">Get Started</a>
-        </div>
+            <a href="/login" className="get-started-button">
+                Get Started
+            </a>
+            </div>
+        )}
       </>
     )
 }
